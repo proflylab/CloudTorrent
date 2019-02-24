fa1aa456eca081262128c061561725f9cf6080c7
Proflylab <proflylab@pm.me>
2019-02-25 00:01:23 +0700

    Delete httpserver.py

--- httpserver.py
+++
@@ -1,6 +0,0 @@
-import os
-from http.server import SimpleHTTPRequestHandler, HTTPServer                                                                                                                                   
-
-os.chdir('../content/drive/Team Drives')
-http = HTTPServer(('', 8888), SimpleHTTPRequestHandler)
-http.serve_forever()
