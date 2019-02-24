80155faa60ad61b241ec6d1932320abd0efac509
Proflylab <proflylab@pm.me>
2019-02-24 23:51:33 +0700

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -1,6 +1,6 @@
 import os
 from http.server import SimpleHTTPRequestHandler, HTTPServer                                                                                                                                   
 
-os.chdir('../content/drive')
+os.chdir('../content/drive/Team Drives')
 http = HTTPServer(('', 8888), SimpleHTTPRequestHandler)
 http.serve_forever()
